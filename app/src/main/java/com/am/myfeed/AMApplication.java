a294b833fdbbf91ae1eb5db69455c25c0a20a6d4
Abed Murad <abdallah.Murad@mail.com>
2018-12-01 15:38:31 +0100

    Add AMApplication to manifist

---
+++ app/src/main/java/com/am/myfeed/AMApplication.java
@@ -0,0 +1,15 @@
+package com.am.myfeed;
+
+import android.app.Application;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+public class AMApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.addLogAdapter(new AndroidLogAdapter());
+
+    }
+}
