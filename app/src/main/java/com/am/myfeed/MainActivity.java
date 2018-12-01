93204bc2b4c9ea41699851ae8135ee767f8a01ad
Abed Murad <abdallah.Murad@mail.com>
2018-12-01 13:48:00 +0100

    Navigation Api Working

--- app/src/main/java/com/am/myfeed/MainActivity.java
+++ app/src/main/java/com/am/myfeed/MainActivity.java
@@ -1,15 +1,22 @@
 package com.am.myfeed;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.am.am_util.activity.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+public class MainActivity extends BaseActivity implements UserFeedFragment.OnFragmentInteractionListener, HeadlinesFeedFragment.OnFragmentInteractionListener, FavoriteArticlesFragment.OnFragmentInteractionListener, ProfileFragment.OnFragmentInteractionListener {
 
+    private NavController mNavController;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -17,12 +24,16 @@ public class MainActivity extends BaseActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_my_feed:
+                    mNavController.navigate(R.id.userFeedFragment);
                     return true;
                 case R.id.navigation_headlines:
+                    mNavController.navigate(R.id.headlinesFeedFragment);
                     return true;
                 case R.id.navigation_favorites:
+                    mNavController.navigate(R.id.favoriteArticlesFragment);
                     return true;
                 case R.id.navigation_profile:
+                    mNavController.navigate(R.id.profileFragment);
                     return true;
             }
             return false;
@@ -33,9 +44,12 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mNavController = Navigation.findNavController(MainActivity.this, R.id.fragment_nav_host);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+    }
 }
