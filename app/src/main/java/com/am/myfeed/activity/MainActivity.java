28abfbcf9777c8c79cd4c1b73a84bd6f65727c86
Abed Murad <abdallah.Murad@mail.com>
2018-12-01 22:27:43 +0100

    More Refactoring

--- app/src/main/java/com/am/myfeed/activity/MainActivity.java
+++ app/src/main/java/com/am/myfeed/activity/MainActivity.java
@@ -1,37 +1,44 @@
 package com.am.myfeed.activity;
 
+import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 
 import com.am.myfeed.R;
-import com.am.myfeed.favoritearticle.FavoriteArticlesFragment;
+import com.am.myfeed.databinding.ActivityMainBinding;
+import com.am.myfeed.favorite.FavoriteArticlesFragment;
+import com.am.myfeed.feed.UserFeedFragment;
 import com.am.myfeed.headlines.HeadlinesFeedFragment;
 import com.am.myfeed.profile.ProfileFragment;
-import com.am.myfeed.userfeed.UserFeedFragment;
+import com.orhanobut.logger.Logger;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends BaseActivity implements UserFeedFragment.OnFragmentInteractionListener, HeadlinesFeedFragment.OnFragmentInteractionListener, FavoriteArticlesFragment.OnFragmentInteractionListener, ProfileFragment.OnFragmentInteractionListener {
+public class MainActivity extends BaseActivity implements
+        UserFeedFragment.OnFragmentInteractionListener,
+        HeadlinesFeedFragment.OnFragmentInteractionListener,
+        FavoriteArticlesFragment.OnFragmentInteractionListener,
+        ProfileFragment.OnFragmentInteractionListener {
 
     private NavController mNavController;
-    BottomNavigationView mBottomNavView;
-
+    private BottomNavigationView mBottomNavView;
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mBottomNavView = findViewById(R.id.bottomNavigationView);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mBottomNavView = mBinding.bottomNavigationView;
         mNavController = Navigation.findNavController(this, R.id.navHostFragment);
         NavigationUI.setupWithNavController(mBottomNavView, mNavController);
     }
 
 
-
     @Override
     public void onFragmentInteraction(Uri uri) {
+        Logger.d("onFragmentInteraction:"+uri.toString());
     }
 }
