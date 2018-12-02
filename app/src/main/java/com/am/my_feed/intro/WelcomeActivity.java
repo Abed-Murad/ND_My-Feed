0458b1f88ef4cd7f8e1d742226385e2e6df39bfc
Abed Murad <abdallah.Murad@mail.com>
2018-12-02 11:29:08 +0100

    Add Deck ViewPager  & Dots Indicators library to project

--- app/src/main/java/com/am/my_feed/intro/WelcomeActivity.java
+++ app/src/main/java/com/am/my_feed/intro/WelcomeActivity.java
@@ -1,15 +1,27 @@
 package com.am.my_feed.intro;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.am.my_feed.R;
+import com.am.my_feed.activity.MainActivity;
+import com.am.my_feed.databinding.ActivityWelcomeBinding;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    private ActivityWelcomeBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
+        mBinding.guestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
