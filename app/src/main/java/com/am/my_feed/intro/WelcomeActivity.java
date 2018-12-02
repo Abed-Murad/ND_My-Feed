69b8dc0691007864d83a91c08a622745888e3e1b
Abed Murad <abdallah.Murad@mail.com>
2018-12-02 14:07:28 +0100

    Activite Welcome Pager

--- app/src/main/java/com/am/my_feed/intro/WelcomeActivity.java
+++ app/src/main/java/com/am/my_feed/intro/WelcomeActivity.java
@@ -10,15 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.am.my_feed.R;
 import com.am.my_feed.activity.MainActivity;
 import com.am.my_feed.databinding.ActivityWelcomeBinding;
+import com.am.my_feed.databinding.FragmentWelcomeBinding;
 import com.am.my_feed.profile.ProfileFragment;
 
 public class WelcomeActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener {
-    private final static int NUM_ITEMS = 3;
+    private final static int NUM_ITEMS = 4;
 
     private ActivityWelcomeBinding mBinding;
 
@@ -53,11 +55,21 @@ public class WelcomeActivity extends AppCompatActivity implements ProfileFragmen
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return ArrayListFragment.newInstance("First", "First Subtitle", R.drawable.ic_bookmark);
+                    return WelcomeFragment.newInstance("Breaking News ",
+                            "We track headlines in 7 categories across over 50 countries, " +
+                                    "and at over a hundred top publications and blogs, in near real time.",
+                            R.drawable.ic_globe);
                 case 1:
-                    return ArrayListFragment.newInstance("Second", "Second Subtitle", R.drawable.ic_bookmark);
+                    return WelcomeFragment.newInstance("Favourite Articles",
+                            "My feed provide an offline mode so you can save your favorite articles and read them latter ", R.drawable.ic_bookmark);
                 case 2:
-                    return ArrayListFragment.newInstance("Third", "Third Subtitle", R.drawable.ic_bookmark);
+                    return WelcomeFragment.newInstance("Search",
+                            "Search articles that mention a specific topic or keyword, we index every article published by over 30,000 news sources and blogs." ,
+                            R.drawable.ic_search);
+                case 3:
+                    return WelcomeFragment.newInstance("Keep in touch",
+                            "Get notification on new articles ",
+                            R.drawable.ic_notifications);
             }
             return null;
         }
@@ -69,25 +81,31 @@ public class WelcomeActivity extends AppCompatActivity implements ProfileFragmen
     }
 
 
-    public static class ArrayListFragment extends Fragment {
+    public static class WelcomeFragment extends Fragment {
+
+        private FragmentWelcomeBinding mBinding;
+        private TextView mTitleTextView;
+        private TextView mSubTitleTextView;
+        private ImageView mIconImageView;
+
         private String mTitle;
         private String mSubtitle;
         private int mIcon;
 
         /**
-         * Create a new instance of CountingFragment, providing "num"
+         * Create a new instance of WelcomeFragment, providing "num"
          * as an argument.
          */
-        public static ArrayListFragment newInstance(String title, String subtitle, int icon) {
-            ArrayListFragment arrayListFragment = new ArrayListFragment();
+        public static WelcomeFragment newInstance(String title, String subtitle, int icon) {
+            WelcomeFragment welcomeFragment = new WelcomeFragment();
 
             // Supply num input as an argument.
             Bundle args = new Bundle();
             args.putString("title", title);
             args.putString("subtitle", subtitle);
             args.putInt("icon", icon);
-            arrayListFragment.setArguments(args);
-            return arrayListFragment;
+            welcomeFragment.setArguments(args);
+            return welcomeFragment;
         }
 
         /**
@@ -102,15 +120,17 @@ public class WelcomeActivity extends AppCompatActivity implements ProfileFragmen
         }
 
         /**
-         * The Fragment's UI is just a simple text view showing its
-         * instance number.
+         * Bind the data to the fragment Ui elements
          */
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_pager_welcome, container, false);
-            View tv = rootView.findViewById(R.id.title);
-            ((TextView) tv).setText("Fragment #" + mTitle);
-            return rootView;
+            mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false);
+
+            mBinding.title.setText(mTitle);
+            mBinding.subtitle.setText(mSubtitle);
+            mBinding.icon.setImageResource(mIcon);
+
+            return mBinding.getRoot();
         }
 
 
