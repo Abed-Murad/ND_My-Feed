1019ad37ad9ff225b308cb1686efab23db318ed2
Abed Murad <abdallah.Murad@mail.com>
2018-12-03 18:45:05 +0100

    Add Search Stuff to manifest

--- app/src/main/java/com/am/my_feed/search/SearchResultsActivity.java
+++ app/src/main/java/com/am/my_feed/search/SearchResultsActivity.java
@@ -1,9 +1,12 @@
 package com.am.my_feed.search;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.am.my_feed.R;
+import com.orhanobut.logger.Logger;
 
 public class SearchResultsActivity extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class SearchResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Logger.d(query);
+        }
     }
 }
