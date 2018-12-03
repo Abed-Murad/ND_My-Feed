aca784fd5ad7185a2ca311ddda373d237888b2c5
Abed Murad <abdallah.Murad@mail.com>
2018-12-03 18:29:05 +0100

    Create SearchResultsActivity

---
+++ app/src/main/java/com/am/my_feed/search/SearchResultsActivity.java
@@ -0,0 +1,15 @@
+package com.am.my_feed.search;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.am.my_feed.R;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
