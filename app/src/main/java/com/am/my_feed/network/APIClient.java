bef4ca460ee2c0ddc42fb7895f48f1c2521bb212
Abed Murad <abdallah.Murad@mail.com>
2018-12-08 03:42:08 +0100

    Add Api Key to the Header of each Requst

--- app/src/main/java/com/am/my_feed/network/APIClient.java
+++ app/src/main/java/com/am/my_feed/network/APIClient.java
@@ -1,6 +1,8 @@
 package com.am.my_feed.network;
 
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,7 +13,21 @@ public class APIClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+
+            /*
+            Create OkHttpClient to Add X-Api-Key to every request made fro the app  
+             */
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(chain -> {
+                Request request = chain.request().newBuilder().addHeader("X-Api-Key", "c130a159f67742cd90c30fc79abebbed").build();
+                return chain.proceed(request);
+            });
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
+                    .build();
         }
         return retrofit;
     }
--- app/src/main/java/com/am/my_feed/network/ApiRequests.java
+++ app/src/main/java/com/am/my_feed/network/ApiRequests.java
@@ -5,21 +5,31 @@ import com.am.my_feed.article.ArticleList;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiRequests {
 
 
-    @GET("top-headlines?country=in&apiKey=c130a159f67742cd90c30fc79abebbed")
+    @GET("top-headlines?country=in")
     Call<ArticleList> getHeadlines();
+
+
+    @GET("everything")
+    @Headers("")
+    Call<Void> getUserFeed(@Query("q") String query,
+                           @Query("from") String from,
+                           @Header("to") String to,
+                           @Header("sortBy") String s,
+                           @Header("Token") String Token);
 
 
     @GET("competitions/{id}/stages")
     Call<Void> getRequests(@Path("id") String competitionId,
-                          @Query("season") String seasonId,
-                          @Header("Signature") String signature,
-                          @Header("Token") String Token);
+                           @Query("season") String seasonId,
+                           @Header("Signature") String signature,
+                           @Header("Token") String Token);
 
 
 }
